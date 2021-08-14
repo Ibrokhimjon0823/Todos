d4fa30798b81ca27b1a7a41b78b1150f00c8bd3e
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2021-08-14 10:32:11 +0500

    serializer committed

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Todo
 
@@ -5,3 +6,9 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'body')
+
+class TodoCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id','title','body']
+        # read_only_fields = ['title','body']
