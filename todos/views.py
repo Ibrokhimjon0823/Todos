1b62fa36c70c4610169c665c7b6f21ec8cff1c8e
Ibrokhimjon0823 <ibrokhimjonmakhamadaliev@gmail.com>
2021-08-14 10:33:15 +0500

    views committed

--- todos/views.py
+++ todos/views.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.shortcuts import render
 from rest_framework import generics, serializers
 from .models import Todo
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer,TodoCompleteSerializer
 
-class ListTodo(generics.ListAPIView):
+class TodoListCreate(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-class DetailTodo(generics.RetrieveAPIView):
+class TodoCompleteSerializer(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
-    serializer_class = TodoSerializer   
+    serializer_class = TodoCompleteSerializer   
